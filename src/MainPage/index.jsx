d8ef16f893d38b2a759ca89532524f3f7599e4ba
Vagner Fabricio <wagnerfabricio@hotmail.com>
2021-11-06 06:59:00 -0300

    Deleted unecessar useEffects and create a new component for API treatment

--- src/MainPage/index.jsx
+++ src/MainPage/index.jsx
@@ -1,12 +1,16 @@
 import Button from "../Button/";
+import theme from "../audio/theme.mp3";
+
 import "./styles.css";
 
 const MainPage = ({ nextPage, handleClick }) => {
+  const song = new Audio(theme);
+
   return (
     <section className="main">
       <h1>Torneio tribruxo</h1>
       <p>Clique no botão para encontrar os feiticeiros!</p>
-      <Button nextPage={nextPage} handleClick={handleClick} playSong={true}>
+      <Button nextPage={nextPage} handleClick={handleClick} song={song}>
         Começar!
       </Button>
     </section>
